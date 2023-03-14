78a5fbf0cfb33175538e404cefd145f010cf5a86
Zhang <martinjzhang@gmail.com>
2023-03-13 22:51:35 -0400

    update

--- experiments/job.curate_data/get_pannot_call.sh
+++ experiments/job.curate_data/get_pannot_call.sh
@@ -80,31 +80,32 @@
 # sbatch -p short -t 0-00:30 -n 1 -c 1 --mem=16000 --open-mode=truncate -o $OUT_PATH/cS2G_other.chr${CHROM}.sbatch.log --wrap "python get_pannot_s3_cS2G.py --pgen_file $PGEN_FILE --pannot cS2G_other --out_path $OUT_PATH"
 # done
 
-# Proxy
-for CHROM in {1..22}
-# for CHROM in 22
-do
-PGEN_FILE=/n/scratch3/users/j/jz286/imp_geno/ukb_imp_chr${CHROM}_v3
-# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/proxy_0_1000
-OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/proxy_0_10000
-# python get_pannot_s1_proxy.py --pgen_file $PGEN_FILE --lb 0 --ub 1000 --out_path $OUT_PATH
-sbatch -p short -t 0-01:00 -n 1 -c 1 --mem=8000 --open-mode=truncate -o $OUT_PATH.proxy.sbatch.log --wrap " \
-python get_pannot_s1_proxy.py --pgen_file $PGEN_FILE --lb 0 --ub 10000 --out_path $OUT_PATH"
-done
-
-# # ldp5_proxy
-# for CHROM in {1..21}
+# # Proxy
+# for CHROM in {1..22}
 # # for CHROM in 22
 # do
 # PGEN_FILE=/n/scratch3/users/j/jz286/imp_geno/ukb_imp_chr${CHROM}_v3
-# LD_FILE=/n/scratch3/users/j/jz286/imp_geno.gdreg_ld/ukb_imp_v3.@_ld.npz
-# # OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_0_100
-# # OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_100_1000
-# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_1000_10000
-# # python get_pannot_s2_ld.py --pgen_file $PGEN_FILE --lb 0 --ub 100 --ld_file $LD_FILE --out_path $OUT_PATH
-# sbatch -p short -t 0-3:00 -n 1 -c 1 --mem=32000 --open-mode=truncate -o $OUT_PATH/chr${CHROM}.sbatch.log --wrap " \
-# python get_pannot_s2_ld.py --pgen_file $PGEN_FILE --lb 1000 --ub 10000 --ld_file $LD_FILE --out_path $OUT_PATH"
+# # OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/proxy_0_1000
+# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/proxy_0_10000
+# # python get_pannot_s1_proxy.py --pgen_file $PGEN_FILE --lb 0 --ub 1000 --out_path $OUT_PATH
+# sbatch -p short -t 0-01:00 -n 1 -c 1 --mem=8000 --open-mode=truncate -o $OUT_PATH.proxy.sbatch.log --wrap " \
+# python get_pannot_s1_proxy.py --pgen_file $PGEN_FILE --lb 0 --ub 10000 --out_path $OUT_PATH"
 # done
+
+# ldp5_proxy
+# for CHROM in {1..21}
+for CHROM in 22
+do
+PGEN_FILE=/n/scratch3/users/j/jz286/imp_geno/ukb_imp_chr${CHROM}_v3
+LD_FILE=/n/scratch3/users/j/jz286/imp_geno.gdreg_ld/ukb_imp_v3.@_ld.npz
+# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_0_100
+OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/ldp5_proxy_0_1000
+# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_100_1000
+# OUT_PATH=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/pannot/pannot_ldp5_proxy_1000_10000
+# python get_pannot_s2_ld.py --pgen_file $PGEN_FILE --lb 0 --ub 100 --ld_file $LD_FILE --out_path $OUT_PATH
+sbatch -p short -t 0-3:00 -n 1 -c 1 --mem=32000 --open-mode=truncate -o $OUT_PATH/chr${CHROM}.sbatch.log --wrap " \
+python get_pannot_s2_ld.py --pgen_file $PGEN_FILE --lb 0 --ub 1000 --ld_file $LD_FILE --out_path $OUT_PATH"
+done
 
 # # Loop
 # # for CHROM in {1..21}
--- experiments/job.curate_data/get_plink.sh
+++ experiments/job.curate_data/get_plink.sh
@@ -1,44 +1,64 @@
 #!/bin/bash
-#SBATCH -c 8
+#SBATCH -c 1
 #SBATCH -N 1
 #SBATCH -t 0-02:00
-#SBATCH --array=1-22
+#SBATCH --array=2
 #SBATCH -p short
-#SBATCH --mem=128000
+#SBATCH --mem=196000
 #SBATCH -o /home/jz286/WES_analysis/GDReg/job_info/hostname_%j.out  
 #SBATCH -e /home/jz286/WES_analysis/GDReg/job_info/hostname_%j.err 
 #SBATCH --mail-type=NONE#SBATCH --mail-type=NONE
 
 
-CHROM=$SLURM_ARRAY_TASK_ID
-OUTPUT_PATH=/n/groups/price/martin/data_GDREG/imp_geno
+# CHROM=$SLURM_ARRAY_TASK_ID
+# OUTPUT_PATH=/n/groups/price/martin/data_GDREG/imp_geno
 
-# Get PLINK file from bgen
-# MAF>0.1% INFO>0.6
-plink2 \
-    --bgen /n/groups/price/UKBiobank/download_500K/ukb_imp_chr${CHROM}_v3.bgen ref-unknown\
-    --sample /n/groups/price/UKBiobank/download_500K/ukb14048_imp_chr1_v3_s487395.sample\
-    --keep /n/groups/price/martin/data_GDREG/imp_geno/unrelated_337K.txt\
-    --extract /n/groups/price/martin/data_GDREG/imp_geno/snp_list/snp_list_chr${CHROM}.MAF_001_INFO_06.txt\
-    --rm-dup force-first\
-    --maj-ref\
-    --geno 0.1\
-    --maf 0.001\
-    --hwe 1e-50\
-    --make-pgen \
-    --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
-    && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3.log
+# # Get PLINK file from bgen
+# # MAF>0.1% INFO>0.6
+# plink2 \
+#     --bgen /n/groups/price/UKBiobank/download_500K/ukb_imp_chr${CHROM}_v3.bgen ref-unknown\
+#     --sample /n/groups/price/UKBiobank/download_500K/ukb14048_imp_chr1_v3_s487395.sample\
+#     --keep /n/groups/price/martin/data_GDREG/imp_geno/unrelated_337K.txt\
+#     --extract /n/groups/price/martin/data_GDREG/imp_geno/snp_list/snp_list_chr${CHROM}.MAF_001_INFO_06.txt\
+#     --rm-dup force-first\
+#     --maj-ref\
+#     --geno 0.1\
+#     --maf 0.001\
+#     --hwe 1e-50\
+#     --make-pgen \
+#     --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
+#     && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3.log
 
-# Recompute the frq info
-plink2 \
-    --pfile ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
-    --freq --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
-        && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3.log
+# # Recompute the frq info
+# plink2 \
+#     --pfile ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
+#     --freq --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
+#         && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3.log
   
-# Create a small vcf file
-SMALL_ID_LIST=/n/groups/price/martin/data_GDREG/imp_geno/unrelated_337K.small.txt
-plink2 \
-    --pfile ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
-    --keep $SMALL_ID_LIST --recode vcf\
-    --out ${OUTPUT_PATH}/vcf/ukb_imp_chr${CHROM}_v3\
-    && rm ${OUTPUT_PATH}/vcf/ukb_imp_chr${CHROM}_v3.log
+# # Create a small vcf file
+# SMALL_ID_LIST=/n/groups/price/martin/data_GDREG/imp_geno/unrelated_337K.small.txt
+# plink2 \
+#     --pfile ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3\
+#     --keep $SMALL_ID_LIST --recode vcf\
+#     --out ${OUTPUT_PATH}/vcf/ukb_imp_chr${CHROM}_v3\
+#     && rm ${OUTPUT_PATH}/vcf/ukb_imp_chr${CHROM}_v3.log
+
+
+
+# Convert to ancestral alleles (chimp): directly converting to pgen
+# CHROM=$SLURM_ARRAY_TASK_ID
+
+CHROM=2
+REF_FILE=/n/groups/price/martin/data_GDREG/UKBimp_337K_MAF001/snp_info/ukb_imp_chr${CHROM}_v3_aa.pvar
+OUTPUT_PATH=/n/scratch3/users/j/jz286/imp_geno_chimp
+# plink2_a37 \
+#     --pfile /n/scratch3/users/j/jz286/imp_geno/ukb_imp_chr${CHROM}_v3\
+#     --ref-allele ${REF_FILE} 10 3\
+#     --make-pgen \
+#     --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3_chimp\
+#     && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3_chimp.log
+
+plink2_a37 \
+    --pfile ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3_chimp\
+    --freq --out ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3_chimp\
+        && rm ${OUTPUT_PATH}/ukb_imp_chr${CHROM}_v3_chimp.log
