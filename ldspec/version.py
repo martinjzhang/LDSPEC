1ba6de8ce62e225157a239ab587e8973686bf9b1
Zhang <martinjzhang@gmail.com>
2023-12-03 16:29:37 -0500

    version

--- ldspec/version.py
+++ ldspec/version.py
@@ -2,5 +2,5 @@
 # 1) we don't load dependencies by storing it in __init__.py
 # 2) we can import it in setup.py for the same reason
 # 3) we can import it into your module module
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __version_info__ = tuple([int(num) for num in __version__.split(".")])
